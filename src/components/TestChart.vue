86711521f79f4d096ab6647cbd18aa5768f6f58c
PaulMagos <paulmagos@icloud.com>
2024-05-16 23:43:42 +0200

    To fix

--- src/components/TestChart.vue
+++ src/components/TestChart.vue
@@ -1,21 +1,16 @@
 <template>
     <Loading v-if="this.loading"></Loading>
-    <v-container v-else id="StreamChart">
+    <v-container v-else id="my_dataviz">
     </v-container>
 </template>
 
-<script>
+<script lang="ts">
 import Loading from './Loading.vue'
-import { defineComponent, toRaw } from 'vue'
-import carbon101 from "@/models/carbon101";
-import googlechartsTheme from "@/models/googlecharts";
+import { defineComponent } from 'vue'
 import axios from 'axios'
 import * as d3 from "d3";
 
-const margin = {top: 20, right: 30, bottom: 30, left: 60},
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom;
-
+const margin = {top: 20, right: 30, bottom: 50, left: 60}
 
 
 export default defineComponent({
@@ -26,49 +21,113 @@ export default defineComponent({
   data(){
     return{
       StreamChart: null,
-      loading: true,
+      loading: false,
       data: null,
       svg: null,
     }
   },
   created(){
-    this.svg = d3.select("#my_dataviz")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            `translate(${margin.left}, ${margin.top})`);
-    this.updateData()
   },
   methods:{
+    get_width(){
+      return window.innerWidth - margin.left - margin.right
+    },
+    get_height(){
+      return window.innerHeight - margin.top - margin.bottom
+    },
     embed(){
-      const x = d3.scaleLinear()
-        .domain(d3.extent(data, function(d) { return d.created_at; }))
-        .range([ 0, width ]);
+      var data = d3.csvParse(this.data)
+
+      const x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) {return d.created_at}))
+        .range([ 0, this.get_width() ]);
       this.svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).ticks(5));
+        .attr("transform", `translate(0, ${(this.get_height())*0.8})`)
+        .call(d3.axisBottom(x).tickSize(-this.get_height()*.7))
+        .select(".domain").remove()
+
+      const keys = data.columns.splice(1)
+
+      this.svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
 
       // Add Y axis
       const y = d3.scaleLinear()
         .domain([-100000, 100000])
-        .range([ height, 0 ]);
-      this.svg.append("g")
-        .call(d3.axisLeft(y));
+        .range([ this.get_height(), 0 ]);
+
+      // color palette
+      const colorer = d3.scaleOrdinal()
+        .domain(keys)
+        .range(d3.schemeDark2);
+
+      //stack the data?
+      const stackedData = d3.stack()
+        .offset(d3.stackOffsetSilhouette)
+        .keys(keys)
+        (data)
+
+      // create a tooltip
+      const Tooltip = this.svg
+        .append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .style("opacity", 0)
+        .style("font-size", 17)
+
+      // Three function that change the tooltip when user hover / move / leave a cell
+      const mouseover = function(event,d) {
+        Tooltip.style("opacity", 1)
+        d3.selectAll(".myArea").style("opacity", .2)
+        d3.select(this)
+          .style("stroke", "black")
+          .style("opacity", 1)
+      }
+      const mousemove = function(event,d,i) {
+        const grp = d.key
+        console.log(grp)
+        Tooltip.text(grp)
+      }
+      const mouseleave = function(event,d) {
+        Tooltip.style("opacity", 0)
+        d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
+      }
+
+      // Area generator
+      const area = d3.area()
+        .x(function(d) { return x(d.data.created_at); })
+        .y0(function(d) { return y(d[0]); })
+        .y1(function(d) { return y(d[1]); })
+
+      // Show the areas
+      this.svg
+        .selectAll("mylayers")
+        .data(stackedData)
+        .join("path")
+          .attr("class", "myArea")
+          .style("fill", function(d) { return colorer(d.key); })
+          .attr("d", area)
+          // .on("mouseover", mouseover)
+          // .on("mousemove", mousemove)
+          // .on("mouseleave", mouseleave)
     },
     async updateData() {
-      this.data = (await axios.get('/get_words?aggregate=false')).data
+      this.data = (await axios.get('/get_words?aggregate=false&pivot=true&min_frequency=10')).data
       while (this.data==undefined){
           setTimeout(() => {
             this.loading = true
           }, (500));
       }
+
       this.loading = false
       this.embed()
     },
     onResize() {
-      this.updateData()
+      this.svg = d3.select("#my_dataviz")
+      .append("svg")
+      .attr("viewBox", [0, 0, this.get_width(),  this.get_height()])
+      .append("g")
+        .attr("transform",
+              `translate(${margin.left}, ${margin.top})`);
     },
   },
   beforeDestroy() {
@@ -76,6 +135,13 @@ export default defineComponent({
   },
   async mounted() {
     this.$nextTick(() => {
+      this.svg = d3.select("#my_dataviz")
+      .append("svg")
+      .attr("viewBox", [0, 0, this.get_width(),  this.get_height()])
+      .append("g")
+        .attr("transform",
+              `translate(${margin.left}, ${margin.top})`);
+      console.log(this.svg)
       window.addEventListener('resize', this.onResize)
       this.updateData()
     })
